e52069ee5ef8e5be239943fb6f8e6a18cbeadbe4
Jayli9 <jayli_97@hotmail.es>
2021-06-24 13:06:38 -0500

    ingresar factura detalles y cabecera

--- src/app/services/facturacion.service.ts
+++ src/app/services/facturacion.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+
+import { VenCabezaFactura } from '../models/VenCabezaFactura'
+import { VenDetalleFact } from '../models/VenDetalleFact'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturacionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  API_URI = 'api/bill'; 
+
+  saveFactura(factura: any): Observable<any> {
+    return this.http.post(`${this.API_URI}`, factura);
+  }
+
 }
